608f3e42a198e1f722830bc63a4619810713c6c8
Tushar Sinha <64056316+tushar67@users.noreply.github.com>
2026-04-25 14:06:42 +0200

    Update page.tsx

--- app/api/blog/[id]/page.tsx
+++ app/api/blog/[id]/page.tsx
@@ -1,9 +1,1 @@
-useEffect(() => {
-  fetch("/api/blog")
-    .then((res) => res.json())
-    .then((data) => {
-      console.log("Blogs:", data);   // 👈 must show array in console
-      setBlogs(data);
-      setLoading(false);
-    });
-}, []);
+
